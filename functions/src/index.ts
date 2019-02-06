8d31fcbe1d4bcedd630f97c947d37c1febe59b17
twelvefish <amingo08131@gmail.com>
2019-02-06 11:04:34 +0800

    新增測試檔案

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -6,3 +6,6 @@ import * as functions from 'firebase-functions';
 // export const helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+export const add = (num1: number, num2: number) => {
+    return num1 + num2
+}
---
+++ functions/test/index.ts
@@ -0,0 +1,13 @@
+import { add } from '../src/index'
+import * as assert from 'assert';
+import * as chai from 'chai';
+
+describe('index.ts', () => {
+    it('add numbers by assert', () => {
+        assert.equal(3, add(1, 2))
+    });
+
+    it('add numbers by chai', () => {
+        chai.expect(add(5, 2)).to.equal(7);
+    });
+});
