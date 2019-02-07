a5ad958da8fb60872a9c323c211eca95e2dc2490
twelvefish <amingo08131@gmail.com>
2019-02-07 09:00:28 +0800

    新增model檔案

---
+++ web/src/app/model.ts
@@ -0,0 +1,125 @@
+export type User = {
+  id: string
+  email: string
+  name: string
+  role: "admin" | "manager" | "staff"
+}
+
+export type Catalog = {
+  id: string
+  name: string
+  index: number
+  messageId: string[]
+  type: "dynamic" | "static"
+  userId: string
+}
+
+export type Message = {
+  id: string
+  title: string
+  content: string
+  thumb: string
+}
+
+export type Member = {
+  id: string
+  name: string
+  title: string
+  division: string
+  department: string
+  lineId: string
+  wechatId: string
+  email: string
+  mobilePhone: string
+  data?: any
+}
+
+export type KendoNode = {
+  id: string
+  text: string
+  parentId?: string
+  type: string
+  data?: any
+}
+
+export type File = {
+  id: string
+  mimeType: string
+  name: string
+  fullName: string
+  parents: string
+  parentsName: parentsName[]
+  sid: number
+  docId: string
+  modifyDate: number
+}
+
+export type parentsName = {
+  id: string
+  name: string
+  fullName: string
+}
+
+export type ChatMessage = {
+  id: string
+}
+
+export type RecordDetail = {
+  id: string
+  channel: "Line" | "WeChat" | "SMS" | "Email"
+  content: string
+  receiver: string
+  receiveTime: number
+  read: boolean
+  urls: Urls[]
+}
+
+export type Group = {
+  id: string
+  memberId: string[]
+  name: string
+  ownerId: string
+}
+
+export type BatchGroup = {
+  id: string
+  members: Content[]
+  name: string
+  ownerId: string
+}
+
+export type Content = {
+  content: string
+  id: string
+  name: string
+}
+
+export type ReviewMessage = {
+  id: string
+  content: string
+  sender: User
+  type: 'immediate' | 'delay'
+  expectTime: number
+  receiverCount: number
+  channel: "Line" | "WeChat" | "SMS" | "Email"
+  urls: Urls[]
+  auditor: User
+  /**
+  * 0: 未處理
+  * 1: 通過 / 未發送
+  * 2: 通過 / 已發送
+  * 3: 未通過
+  */
+  state: 0 | 1 | 2 | 3
+}
+
+export type Urls = {
+  name: string
+  url: string
+}
+
+export type Receiver = {
+  id: string
+  index: number
+  data: any[]
+}
